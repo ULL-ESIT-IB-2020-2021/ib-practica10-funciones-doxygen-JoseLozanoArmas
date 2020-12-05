9483ce18413f2b7cbb774f98533d44eca32b50da
usuario <alu0101392561@ull.edu.es>
2020-12-05 11:00:43 +0000

    Ejercicio Criptografia

---
+++ cripto/cripto_main.cc
@@ -0,0 +1,16 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica
+  *
+  * @file cripto_main.cc
+  * @author José Lozano Armas alu0101392561@ull.edu.es
+  * @date -------------------------
+  * @brief El programa al introducirle un fichero de texto, se encargará de encriptarlo o desencriptarlo 
+  *        en base a los argumentos que se le pasen. 
+  * @bug 
+  * @see 
+  */
+#include <iostream>
+
